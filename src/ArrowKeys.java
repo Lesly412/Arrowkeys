8b88d768ed8c4c6d2b5b19979ffc6444cb1a8c92
Lesly412 <arianelesly412@gmail.com>
2022-04-30 17:09:48 -0400

    Initial commit

---
+++ src/ArrowKeys.java
@@ -0,0 +1,46 @@
+import javax.swing.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class ArrowKeys {
+    public ArrowKeys {
+        JFrame frame = new JFrame();
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400,400);
+        frame.setFocusable(true);
+
+        JPanel panel = new JPanel();
+        JLabel up = new JLabel();
+        JLabel down = new JLabel();
+        JLabel left = new JLabel();
+        JLabel right = new JLabel();
+
+        panel.add(up);
+        panel.add(down);
+        panel.add(left);
+        panel.add(right);
+
+        up.setText("Up : 0 ");
+        up.setText("Down : 0 ");
+        up.setText("Left : 0 ");
+        up.setText("Right : 0 ");
+
+        frame.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+
+            }
+        });
+    }
+}
